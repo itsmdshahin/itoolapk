f827f30f45b55e2601afb2de82d3d1bc572b5d54
itsmdshahin <mdshahinurrahman8@gmail.com>
2023-12-25 10:20:49 +0600

    all apk api added

--- src/components/AllApps/AllApps.js
+++ src/components/AllApps/AllApps.js
@@ -1,49 +1,126 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { StyleSheet, View, Text, Image, FlatList, TouchableOpacity, Linking } from 'react-native';
+
+const appsData = [
+  {
+    key: '1',
+    name: 'WhatsApp Messenger',
+    image: 'path-to-whatsapp-image',
+    downloadUrl: 'https://d.apkpure.com/b/APK/com.android.chrome?version=latest',
+    rating: '8.1',
+  },
+  {
+    key: '2',
+    name: 'WhatsApp Messenger',
+    image: 'path-to-whatsapp-image',
+    downloadUrl: 'https://d.apkpure.com/b/APK/com.android.chrome?version=latest',
+    rating: '8.2',
+  },
+  {
+    key: '3',
+    name: 'WhatsApp Messenger',
+    image: 'path-to-whatsapp-image',
+    downloadUrl: 'https://d.apkpure.com/b/APK/com.android.chrome?version=latest',
+    rating: '8.2',
+  },
+  {
+    key: '4',
+    name: 'WhatsApp Messenger',
+    image: 'path-to-whatsapp-image',
+    downloadUrl: 'https://d.apkpure.com/b/APK/com.android.chrome?version=latest',
+    rating: '8.2',
+  },
+  {
+    key: '5',
+    name: 'WhatsApp Messenger',
+    image: 'path-to-whatsapp-image',
+    downloadUrl: 'https://d.apkpure.com/b/APK/com.android.chrome?version=latest',
+    rating: '8.2',
+  },
+  {
+    key: '6',
+    name: 'WhatsApp Messenger',
+    image: 'path-to-whatsapp-image',
+    downloadUrl: 'https://d.apkpure.com/b/APK/com.android.chrome?version=latest',
+    rating: '8.2',
+  },
+  {
+    key: '7',
+    name: 'WhatsApp Messenger',
+    image: 'path-to-whatsapp-image',
+    downloadUrl: 'https://d.apkpure.com/b/APK/com.android.chrome?version=latest',
+    rating: '8.2',
+  },
+  // Add other apps following the same structure
+];
+
+const openDownloadLink = (downloadUrl) => {
+  Linking.openURL(downloadUrl).catch((err) => console.error('An error occurred', err));
+};
+
+const AppIcon = ({ name, image, rating, downloadUrl }) => (
+  <TouchableOpacity style={styles.appContainer} onPress={() => openDownloadLink(downloadUrl)}>
+    <Image source={{ uri: image }} style={styles.appIcon} />
+    <Text style={styles.appName}>{name}</Text>
+    <Text style={styles.appRating}>{rating}</Text>
+    <Text style={styles.downloadButton}>Download</Text>
+  </TouchableOpacity>
+);
 
 const AllApps = () => {
-    return (
-        <View>
-            <Text>AllApps</Text>
-            <View style={styles.header}>
-                <Text style={styles.headerText}>Welcome to Our App</Text>
-            </View>
-            <View style={styles.content}>
-                <Text style={styles.contentText}>Explore the app to find great features!</Text>
-            </View>
-        </View>
-    )
-}
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Popular Apps In Last 24 Hours</Text>
+      <FlatList
+        data={appsData}
+        renderItem={({ item }) => (
+          <AppIcon name={item.name} image={item.image} rating={item.rating} downloadUrl={item.downloadUrl} />
+        )}
+        numColumns={3}
+        keyExtractor={(item) => item.key}
+      />
+    </View>
+  );
+};
 
-export default AllApps
-
+export default AllApps;
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#F0F0F0',
-    },
-    header: {
-      paddingTop: 60,
-      paddingBottom: 20,
-      backgroundColor: '#4A90E2',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    headerText: {
-      color: '#FFFFFF',
-      fontSize: 24,
-      fontWeight: 'bold',
-    },
-    content: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      padding: 20,
-    },
-    contentText: {
-      fontSize: 18,
-      color: '#333333',
-      textAlign: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginVertical: 10,
+  },
+  appContainer: {
+    margin: 5,
+    alignItems: 'center',
+    backgroundColor: '#f0f0f0',
+    padding: 10,
+    borderRadius: 8,
+  },
+  appIcon: {
+    width: 80,
+    height: 80,
+  },
+  appName: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    color: '#000',
+    marginTop: 5,
+  },
+  appRating: {
+    fontSize: 12,
+    color: 'grey',
+  },
+  downloadButton: {
+    marginTop: 10,
+    color: '#007bff',
+    fontWeight: 'bold',
+  },
 });
